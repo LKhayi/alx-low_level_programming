ea4e18ca5693c661160a661ee110040549c61a72
LKhayi <lamiaakhayi@gmail.com>
2023-03-17 17:56:11 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * return: 0 always (Success)
+ **/
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
